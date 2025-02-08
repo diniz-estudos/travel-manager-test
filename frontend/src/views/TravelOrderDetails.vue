03793c07ea4c5276945717f08c67dcfde323ce0e
Eduvyres Diniz <eduvyres@gmail.com>
2025-02-08 17:00:45 -0300

    feat: melhora a exibição dos detalhes do pedido de viagem com novas informações e layout

--- frontend/src/views/TravelOrderDetails.vue
+++ frontend/src/views/TravelOrderDetails.vue
@@ -7,15 +7,87 @@
         <div v-else>
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Pedido #{{ order.id }}</h5>
-                    <p><strong>Usuário:</strong> {{ order.user.name }}</p>
-                    <p><strong>Destino:</strong> {{ order.destino }}</p>
-                    <p><strong>Data de Ida:</strong> {{ formatDate(order.data_ida) }}</p>
-                    <p><strong>Data de Volta:</strong> {{ formatDate(order.data_volta) }}</p>
-                    <p><strong>Status:</strong> <span
-                            :class="['badge', 'bg-' + (order.status === 'aprovado' ? 'success' : order.status === 'cancelado' ? 'danger' : 'info')]">{{
-                            order.status }}</span></p>
-                    <button class="btn btn-secondary" @click="goBack">Voltar</button>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="alert alert-info" role="alert">
+                                <strong>Atenção!</strong> Este é um ambiente de demonstração, portanto, os pedidos de
+                                viagem
+                                não são reais.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row small">
+                        <div class="col-md-4">
+                            <span><strong>Pedido #</strong></span>
+                        </div>
+                        <div class="col-md-4">
+                            <span><strong>Status</strong></span>
+                        </div>
+                        <div class="col-md-4">
+                            <span><strong>Utlima atualização</strong></span>
+                        </div>
+                    </div>
+                    <div class="row lead mb-3">
+                        <div class="col-md-4 text-center">
+                            <span><strong>{{ String(order.id).padStart(10, '0') }}</strong></span>
+                        </div>
+                        <div class="col-md-4">
+                            <span
+                                :class="['badge', 'bg-' + (order.status === 'aprovado' ? 'success' : order.status === 'cancelado' ? 'danger' : 'info')]">{{
+                                    order.status }}
+                            </span>
+                        </div>
+                        <div class="col-md-4">
+                            <span>{{ formatDate(order.updated_at) }}</span>
+                        </div>
+                    </div>
+
+                    <div class="row small">
+                        <div class="col-md-4">
+                            <span><strong>Usuário</strong></span>
+                        </div>
+                        <div class="col-md-4">
+                            <span><strong>E-Mail</strong></span>
+                        </div>
+                        <div class="col-md-4">
+                            <span><strong>Destino</strong></span>
+                        </div>
+                    </div>
+                    <div class="row lead mb-3">
+                        <div class="col-md-4">
+                            <span>{{ order.user.name }}</span>
+                        </div>
+                        <div class="col-md-4">
+                            <span>{{ order.user.email }}</span>
+                        </div>
+                        <div class="col-md-4">
+                            <span>{{ order.destino }}</span>
+                        </div>
+                    </div>
+
+                    <div class="row small">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4">
+                            <span><strong>Data de Ida</strong></span>
+                        </div>
+                        <div class="col-md-4">
+                            <span><strong>Data de Volta</strong></span>
+                        </div>
+                    </div>
+                    <div class="row lead">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4">
+                            <span>{{ formatDate(order.data_ida) }}</span>
+                        </div>
+                        <div class="col-md-4">
+                            <span>{{ formatDate(order.data_volta) }}</span>
+                        </div>
+                    </div>
+                    <div class="row small">
+                        <div class="col-md-12">
+                            <button class="btn btn-secondary" @click="goBack">Voltar</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
